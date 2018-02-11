021644a3c6ffdb7882c94f830af7dae8febbcf69
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2018-02-10 20:11:11 -0500

    modifying entropy alg

--- id3/ID3.py
+++ id3/ID3.py
@@ -1,15 +1,21 @@
 import math
+import numpy as np
 
 class ID3(object):
     def __init__(self, data):
         self.data = data
         self.es = None
 
-    def system_entropy(self, oracle):
-        true = 0
-        false = 0
-        size = len(oracle)
+    def entropy(self, s, k):
+        """
 
+        :param s: collection of data
+        :param k: categories numpy array [all values][the categories]
+        :return:
+        """
+        #  Cardinality of the collection.
+        sc = s.shape[0]
+        
         #  Find how many T/F values there are.
         for answer in oracle:
             if answer == 1:
